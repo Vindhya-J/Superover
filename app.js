8d1b0c1f0996c6145713ff75f75047aa33cb7058
Vindhya J <you@example.com>
2020-11-20 19:03:39 +0530

    done

--- app.js
+++ app.js
@@ -32,14 +32,17 @@ function selectToss(){
 function updateName(){
     console.log("Team 1 name - "+team1.name);
     console.log("Team 2 name - "+team2.name);
+    document.getElementById("team1name").textContent=team1.name;
+    document.getElementById("team2name").textContent=team2.name;
 }
 
-function updateButtontext(){
-   console.log( `${toss===1?team1.name:team2.name} BATTING`)
-
-   if(team1.runs.length==6 && team2.runs.length==6){
+function updateButtontext(){ 
+    var button =document.getElementById("strikebutton")
+    var result=document.getElementById("result")
+    if(team1.runs.length==6 && team2.runs.length==6){
        console.log("Both team finished Superover");
-        if(team1.score>team2.score){
+       button.remove();
+        /*if(team1.score>team2.score){
             console.log(team1.name+" wins");
         }
         else if(team1.score==team2.score){
@@ -47,22 +50,24 @@ function updateButtontext(){
         }
         else{
             console.log(team2.name+" wins");
-        }
+        }*/
+        console.log(team1.score===team2.score?"Draw":`${team1.score>team2.score?team1.name:team2.name} Wins`)
+        result.textContent=team1.score==team2.score?"Draw":`${team1.score>team2.score?team1.name:team2.name} Wins`
    }
    else{
        toss=team1.runs.length===6?2:
        team2.runs.length===6?1:toss;
    }
+   console.log( `${toss===1?team1.name:team2.name} BATTING`)
+   document.getElementById("strikebutton").textContent= `${toss===1?team1.name:team2.name} BATTING`
 }
 
 handlestrike=()=>{
-    console.log("onclick works")
     var runs=score[Math.floor(Math.random()*score.length)]
-    console.log(runs)
     if(toss==1){
-        team1.runs.push(runs)
-        console.log(runs)
-        team1.score=calculatescore(team1.runs)
+        team1.runs.push(runs);
+        console.log(runs);
+        team1.score=calculatescore(team1.runs);
     }
     else{
         team2.runs.push(runs)
@@ -74,12 +79,29 @@ handlestrike=()=>{
 }
 
 function updateScores(){
-    console.log("RCB Score : "+team1.score)
-    console.log("KXIP Score : "+team2.score)
+    console.log("Team 1 Score : "+team1.score);
+    console.log("Team 2 Score : "+team2.score);
+    document.getElementById("team1score").textContent=team1.score;
+    document.getElementById("team2score").textContent=team2.score;
+    updateRuns();
 }
 
 var calculatescore=(runs)=>{
     return runs.map(num=>{
         return num;
     }).reduce((total,num)=>total+num);
+}
+
+function updateRuns(){
+    var team1runs = document.getElementById("team1runs").children
+
+    team1.runs.forEach((run,index)=>{
+        team1runs[index].textContent=run
+    });
+
+    var team2runs = document.getElementById("team2runs").children
+
+    team2.runs.forEach((run,index)=>{
+        team2runs[index].textContent=run
+    });
 }
